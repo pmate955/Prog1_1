bc9b1a8a739c69741aef997689d3bd7019176b01
Mate Pinter <pmate955@gmail.com>
2018-09-28 10:58:36 +0200

    wip

--- src/GUI/GameFrame.java
+++ src/GUI/GameFrame.java
@@ -6,25 +6,57 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import Map.GameMap;
+
 public class GameFrame extends JFrame implements ActionListener {
 	
 	private JLabel[][] labels;
+	private GameMap map;
+	private int size;
+	private ImageIcon dirt;
+	private ImageIcon rock;
+	private ImageIcon water;
+	private ImageIcon robot;
+	private ImageIcon station;
+	private ImageIcon rail;
 	
-	public GameFrame(){
+	public GameFrame(int size){
 		this.setTitle("Jatekunk");
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setSize(400, 400);
 		this.setLayout(new BorderLayout());
-		this.labels = new JLabel[6][6];
+		this.labels = new JLabel[size][size];
+		this.size = size;
+		
+		try {
+			this.dirt = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/dirt.PNG")));
+			this.rock = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/rock.PNG")));
+			this.water = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/water.PNG")));
+			this.robot = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/robot.PNG")));
+			this.station = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/station.PNG")));
+			this.rail = new ImageIcon((BufferedImage)ImageIO.read(this.getClass().getResource("/Icons/rail.PNG")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.add(generateGamePanel(), BorderLayout.CENTER);
 		this.add(generateButtons(),BorderLayout.SOUTH);
+		this.map =  new GameMap(size);
+		this.draw();
 		this.setVisible(true);
+		
+		this.pack();
 	}
 
 	private JPanel generateButtons() {
@@ -34,12 +66,31 @@ public class GameFrame extends JFrame implements ActionListener {
 		JButton down = new JButton("Down");
 		JButton left = new JButton("Left");
 		JButton right = new JButton("Right");
+		JButton dinamite = new JButton("Dinamite");
+		JButton bridge = new JButton("Bridge");
+		JButton start = new JButton("Start");
+		JButton stop = new JButton("Stop");
 		up.setActionCommand("up");
 		down.setActionCommand("down");
+		left.setActionCommand("left");
+		right.setActionCommand("right");
+		
 		up.addActionListener(this);
 		down.addActionListener(this);
+		left.addActionListener(this);
+		right.addActionListener(this);
+		dinamite.addActionListener(this);
+		bridge.addActionListener(this);
+		start.addActionListener(this);
+		stop.addActionListener(this);
 		jp.add(up);
 		jp.add(down);
+		jp.add(left);
+		jp.add(right);
+		jp.add(dinamite);
+		jp.add(bridge);
+		jp.add(start);
+		jp.add(stop);
 		return jp;
 	}
 
@@ -48,8 +99,8 @@ public class GameFrame extends JFrame implements ActionListener {
 		jp.setLayout(new GridLayout(6,6));
 		for(int x = 0; x < 6; x++){
 			for(int y = 0; y < 6; y++){
-				JLabel actual = new JLabel("0");
-				labels[x][y] = actual;
+				JLabel actual = new JLabel();
+				labels[x][y] = actual;				
 				jp.add(actual);
 			}
 		}
@@ -62,7 +113,39 @@ public class GameFrame extends JFrame implements ActionListener {
 		// TODO Auto-generated method stub
 		JButton act = (JButton)e.getSource();
 		System.out.println(act.getActionCommand());
-		
+		String cmd = act.getActionCommand();
+		if(cmd.equals("up")){
+			if(map.doStep(cmd)){
+				System.out.println("Lefutott");
+			} else {
+				System.out.println("Nem");
+			};
+		} else if(cmd.equals("down")){
+			
+		}
+		this.draw();
+	}
+	
+	public void draw(){
+		for(int x = 0; x < size; x++){
+			for(int y = 0; y < size; y++){
+				int actual = map.map[x][y];
+				if(actual==1){
+					labels[x][y].setIcon(dirt);
+				} else if(actual == 2){
+					labels[x][y].setIcon(rock);
+				} else if(actual == 3){
+					labels[x][y].setIcon(water);
+				} else if(actual == 5){
+					labels[x][y].setIcon(robot);
+				} else if(actual == 10){
+					labels[x][y].setIcon(station);
+				} else if(actual == 15){
+					labels[x][y].setIcon(rail);
+				}
+			}
+		}
+		this.repaint();	
 	}
 
 }
--- src/Map/GameMap.java
+++ src/Map/GameMap.java
@@ -1,16 +1,83 @@
 package Map;
 
+import java.awt.Point;
+import java.io.*;
+import java.net.URISyntaxException;
+
 public class GameMap {
 
-	private int[][] map;			//1- sima, 2- szikla, 3- viz, 5 - robot, 10 - allomas
+	public int[][] map;			//1- sima, 2- szikla, 3- viz, 5 - robot, 10 - allomas, 15 - sin
 	private int size;
-	
+	private Point robot;
 	
 	public GameMap(int size) {
 		this.size = size;
 		this.map = new int[size][size];
+		this.robot = new Point(0,0);
+		this.readMap();
 	}
 	
+	public void generate(){
+		for(int x = 0; x < size; x++){
+			for(int y = 0; y < size; y++){
+				int random = (int)(Math.random()*3)+1;
+				map[x][y] = random;
+			}
+		}
+		this.printMap();
+	}
 	
+	public void printMap(){
+		for(int x = 0; x < size; x++){
+			for(int y = 0; y < size; y++){				
+				System.out.print(map[x][y] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void readMap(){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(this.getClass().getResource("/Levels/lvl1.txt").toURI())));
+			String s = br.readLine();
+			int row = 0;
+			while(s != null){
+				String[] d = s.split(" ");
+				for(int i = 0; i < d.length; i++){
+					int act = Integer.parseInt(d[i]);
+					map[row][i] = act;
+					if(act == 5){
+						this.robot.setLocation(row, i);
+					}
+				}
+				s = br.readLine();
+				row++;
+			}
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e){
+			
+		}
+	}
+	
+	public boolean doStep(String direction){
+		int x = robot.x;
+		int y = robot.y;
+		if(direction.equals("up")){
+			if(x > 0){
+				if(map[x-1][y] == 1 || map[x-1][y] == 10){
+					map[x][y] = 15;
+					map[x-1][y] = 5;
+					robot.x--;
+				} else return false;
+			} else return false;
+		}
+		return true;
+	}
 	
 }
